a61cfb05c4b32ceab515b9ec461ed563744e873a
zhan70 <zhan70y@yandex.ru>
2026-03-17 18:07:43 +0000

    build

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from '../../header/header';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [Header],
+  imports: [Header,RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
